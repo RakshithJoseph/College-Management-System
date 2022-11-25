a5843c741814156996dad419386ef0fe7ed34f26
RakshithJoseph <ui20cs54@iiitsurat.ac.in>
2022-11-25 15:09:13 +0530

    a

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -119,7 +119,7 @@ const Login = () => {
 
   return (
     <>
-      <Navbar title="Faculty and Student Login" showText="" />
+      <Navbar title="Faculuhty and Student Login" showText="" />
       <section className="loginContainer">
         <div className="loginCard">
           <img id="loginImg" src={require("../assets/classPng.png")} alt="" />
